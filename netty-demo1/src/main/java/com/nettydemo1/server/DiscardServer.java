6f0773ff8efc20cc62e531bb547c2b0e7f07e685
Administrator <1474886853@qq.com>
2024-03-05 19:23:56 +0800

    1

--- netty-demo1/src/main/java/com/nettydemo1/server/DiscardServer.java
+++ netty-demo1/src/main/java/com/nettydemo1/server/DiscardServer.java
@@ -1,4 +1,47 @@
 package com.nettydemo1.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.sctp.nio.NioSctpServerChannel;
+import io.netty.channel.socket.SocketChannel;
+
 public class DiscardServer {
+    private int port;
+
+    public DiscardServer(int port) {
+        this.port=port;
+    }
+
+    private void run() throws Exception {
+        EventLoopGroup boss = new NioEventLoopGroup();  
+        EventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(boss, worker)
+                    .channel(NioSctpServerChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            ChannelFuture sync = b.bind(port).sync();
+            sync.channel().closeFuture().sync();
+        }finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port=8080;
+        new DiscardServer(port).run();
+    }
+
 }
