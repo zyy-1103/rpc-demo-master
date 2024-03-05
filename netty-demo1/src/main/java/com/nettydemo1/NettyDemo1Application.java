a57819e3bc3ca00436e4148ea0b18148675700f7
Administrator <1474886853@qq.com>
2024-03-05 14:59:07 +0800

    1

---
+++ netty-demo1/src/main/java/com/nettydemo1/NettyDemo1Application.java
@@ -0,0 +1,13 @@
+package com.nettydemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyDemo1Application.class, args);
+    }
+
+}
---
+++ netty-demo1/src/test/java/com/nettydemo1/NettyDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nettydemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
